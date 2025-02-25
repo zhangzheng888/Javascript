9700912bd2617b7f74aadd375c84ee4b32fbcd20
Kevin Zhang <zhangzheng888@gmail.com>
2025-02-24 21:21:41 -0800

    Create Profile Lookup JavaScript file

---
+++ Data Types & Variables/201 Profile Lookup.js
@@ -0,0 +1,69 @@
+/*
+We have an array of objects representing different people in our contacts lists.
+
+A lookUpProfile function that takes name and a property (prop) as arguments has been pre-written for you.
+
+The function should check if name is an actual contact's firstName and the given property (prop) is a property of that contact.
+
+If both are true, then return the "value" of that property.
+
+If name does not correspond to any contacts then return the string No such contact.
+
+If prop does not correspond to any valid properties of a contact found to match name then return the string No such property.
+*/
+
+const contacts = [
+    {
+      firstName: "Akira",
+      lastName: "Laine",
+      number: "0543236543",
+      likes: ["Pizza", "Coding", "Brownie Points"],
+    },
+    {
+      firstName: "Harry",
+      lastName: "Potter",
+      number: "0994372684",
+      likes: ["Hogwarts", "Magic", "Hagrid"],
+    },
+    {
+      firstName: "Sherlock",
+      lastName: "Holmes",
+      number: "0487345643",
+      likes: ["Intriguing Cases", "Violin"],
+    },
+    {
+      firstName: "Kristian",
+      lastName: "Vos",
+      number: "unknown",
+      likes: ["JavaScript", "Gaming", "Foxes"],
+    },
+  ];
+  
+  function lookUpProfile(name, prop) {
+    for (let i = 0; i < contacts.length; i++) {
+      if (contacts[i].firstName === name) {
+        if (contacts[i].hasOwnProperty(prop)) {
+          return contacts[i][prop];
+        } else {
+          return "No such property";
+        }
+      }
+      else {
+        return "No such contact";
+      }
+    }
+  }
+  
+lookUpProfile("Akira", "likes"); // ["Pizza", "Coding", "Brownie Points"]
+
+lookUpProfile("Kristian", "lastName") // Vos
+
+lookUpProfile("Sherlock", "likes") // ["Intriguing Cases", "Violin"]
+
+lookUpProfile("Harry", "likes") // ["Hogwarts", "Magic", "Hagrid"]
+
+lookUpProfile("Bob", "number") // No such contact
+
+lookUpProfile("Bob", "potato") // No such contact
+
+lookUpProfile("Akira", "address") // No such property
