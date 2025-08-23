615a92804db2786482a872c3ef3d6a4524ba28a7
Kevin Zhang <zhangzheng888@gmail.com>
2025-08-23 10:08:03 -0700

    Create Repeat a String Repeat a String Javascripe file

---
+++ Data Types & Variables/309 Repeat a String Repeat a String.js
@@ -0,0 +1,21 @@
+/*
+Repeat a given string str (first argument) for num times (second argument). 
+Return an empty string if num is not a positive number. 
+For the purpose of this challenge, do not use the built-in .repeat() method.
+*/
+
+function repeatStringNumTimes(str, num) {
+    for (var i = 0; i < num; i++) {
+        // If num is less than or equal to 0, return an empty string
+        if (num <= 0) {
+            return '';
+        }
+        // Concatenate the string to itself num times
+        str += str;
+    }
+    // Return the final repeated string
+    return str
+}
+
+repeatStringNumTimes('abc', 3);
+// Should return 'abcabcabc'.
