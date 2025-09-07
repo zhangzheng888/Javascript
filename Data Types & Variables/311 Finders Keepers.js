8a1d30980620fee4ed68463525cc4d46f2813345
Kevin Zhang <zhangzheng888@gmail.com>
2025-09-07 07:52:38 -0700

    Add Finders Keepers Javascript file example

--- Data Types & Variables/311 Finders Keepers.js
+++ Data Types & Variables/311 Finders Keepers.js
@@ -17,3 +17,6 @@ findElement([1, 2, 3, 4], num => num % 2 === 0);
 
 findElement([1, 3, 5, 8, 9, 10], function(num) { return num % 2 === 0; });
 // Should return 8.
+
+findElement([1, 3, 5, 9], function(num) { return num % 2 === 0; });
+// Should return undefined.
