244c75bbd8e1bcde2708720a3a728080a1cbf111
Kevin Zhang <zhangzheng888@gmail.com>
2025-09-08 07:46:08 -0700

    Create Boo Whoo Javascript file

---
+++ Data Types & Variables/312 Boo Who.js
@@ -0,0 +1,15 @@
+/*
+Check if a value is classified as a boolean primitive. Return true or false.
+
+Boolean primitives are true and false.
+*/
+
+function booWho(bool) {
+    if (typeof bool !== 'boolean') {
+        return false;
+    }
+    return true;
+}
+
+booWho(null);
+// Should return false.
