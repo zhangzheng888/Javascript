fb527626eed0408039993ed846efe30bf1a87e4a
Kevin Zhang <zhangzheng888@gmail.com>
2025-09-18 20:12:07 -0700

    Create Title Case a Sentence Javascript file

---
+++ Data Types & Variables/313 Title Case a Sentence.js
@@ -0,0 +1,13 @@
+/*
+Return the provided string with the first letter of each word capitalized. Make sure the rest of the word is in lower case.
+
+For the purpose of this exercise, you should also capitalize connecting words like the and of.
+*/
+
+function titleCase(str) {
+    let words = str.toLowerCase().split(' ');
+    for (let i = 0; i < words.length; i++) {
+        words[i] = words[i][0].toUpperCase() + words[i].slice(1);
+    }
+    return words.join(' ');
+}
