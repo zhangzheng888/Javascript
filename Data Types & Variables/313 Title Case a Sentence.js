33fa7b645404d00c93ae446e81b06cf54d591411
Kevin Zhang <zhangzheng888@gmail.com>
2025-09-21 11:31:13 -0700

    Update  Title Case a Sentence Javascript file with example

--- Data Types & Variables/313 Title Case a Sentence.js
+++ Data Types & Variables/313 Title Case a Sentence.js
@@ -17,3 +17,6 @@ titleCase("I'm a little tea pot");
 
 titleCase("sHoRt AnD sToUt");
 // should return "Short And Stout"
+
+titleCase("HERE IS MY HANDLE HERE IS MY SPOUT");
+// should return "Here Is My Handle Here Is My Spout"
