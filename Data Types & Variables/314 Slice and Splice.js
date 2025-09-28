f5d7455425ad4cef098bc551cdea62acb33d447d
Kevin Zhang <zhangzheng888@gmail.com>
2025-09-28 10:22:23 -0700

    Update Slice and Splice Javascript file

--- Data Types & Variables/314 Slice and Splice.js
+++ Data Types & Variables/314 Slice and Splice.js
@@ -21,3 +21,6 @@ frankenSplice([1, 2, 3], [4, 5], 1);
 
 frankenSplice([1, 2], ["a", "b"], 1);
 // should return ["a", 1, 2, "b"]
+
+frankenSplice(["claw", "tentacle"], ["head", "shoulders", "knees", "toes"], 2);
+// should return ["head", "shoulders", "claw", "tentacle", "knees", "toes"]
