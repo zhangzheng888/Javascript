aa5bda7ca1d44ca1907540e5758e1b71f9dffa30
Kevin Zhang <zhangzheng888@gmail.com>
2025-10-04 16:25:39 -0700

    Create Falsy Bouncer Javascript file

---
+++ Data Types & Variables/315 Falsy Bouncer.js
@@ -0,0 +1,7 @@
+/*
+Remove all falsy values from an array. Return a new array; do not mutate the original array.
+
+Falsy values in JavaScript are false, null, 0, "", undefined, and NaN.
+
+Hint: Try converting each value to a Boolean.
+*/
