80272a124c2c560579571e4e6de77e5331712fc4
Kevin Zhang <zhangzheng888@gmail.com>
2025-11-10 22:48:14 -0800

    Update Mutations Javascript file with example

--- Data Types & Variables/317 Mutations.js
+++ Data Types & Variables/317 Mutations.js
@@ -7,3 +7,15 @@ The arguments ["hello", "hey"] should return false because the string hello does
 
 Lastly, ["Alien", "line"], should return true because all of the letters in line are present in Alien.
 */
+
+
+function mutation(arr) {
+    let first = arr[0].toLowerCase();
+    let second = arr[1].toLowerCase();
+    for (let i = 0; i < second.length; i++) {
+        if (first.indexOf(second[i]) === -1) {
+            return false;
+        }
+    }
+    return true;
+}
