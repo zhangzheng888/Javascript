2bf0e944f363cf8c98120e286f508083d273c199
Kevin Zhang <zhangzheng888@gmail.com>
2025-11-24 06:29:13 -0800

    Create Chunky Monkey Javascript file

---
+++ Data Types & Variables/318 Chunky Monkey.js
@@ -0,0 +1,8 @@
+/* 
+Write a function that splits an array (first argument) into groups the 
+length of size (second argument) and returns them as a two-dimensional array.
+*/
+
+function chunkArrayInGroups(arr, size) {
+  return arr;
+}
