49a49a268fe2cfd4debc1fd9e87a7175652e4dcf
Kevin Zhang <zhangzheng888@gmail.com>
2025-12-03 08:57:04 -0800

    Update Chunky Monkey Javascript file with example

--- Data Types & Variables/318 Chunky Monkey.js
+++ Data Types & Variables/318 Chunky Monkey.js
@@ -28,3 +28,6 @@ chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6], 3);
 
 chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 4);
 // Should return [[0, 1, 2, 3], [4, 5, 6, 7], [8]].
+
+chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 2);
+// Should return [[0, 1], [2, 3], [4, 5], [6, 7], [8]].
