7b95385e1a8bd63003fac9aab2576e76fa57e36b
Kevin Zhang <zhangzheng888@gmail.com>
2025-12-07 21:20:12 -0800

    Create Use Dot Notation to Access the Properties Javascript Object file

---
+++ Data Types & Variables/320 Use Dot Notation to Access the Properties of an Object.js
@@ -0,0 +1,18 @@
+/*
+The last challenge created an object with various properties. Now you'll see how to access the values of those properties. Here's an example:
+
+let duck = {
+  name: "Aflac",
+  numLegs: 2
+};
+console.log(duck.name);
+Dot notation is used on the object name, duck, followed by the name of the property, name, to access the value of Aflac.
+*/
+
+let dog = {
+  name: "Buddy",
+  numLegs: 4
+};
+
+console.log(dog.name);
+console.log(dog.numLegs);
