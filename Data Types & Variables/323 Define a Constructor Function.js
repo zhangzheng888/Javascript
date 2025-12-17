31c73c5d6860ccd9fbcf649d59354ebe446d3984
Kevin Zhang <zhangzheng888@gmail.com>
2025-12-17 00:04:20 -0800

    Update Define a Constructor Function JavaScript file

--- Data Types & Variables/323 Define a Constructor Function.js
+++ Data Types & Variables/323 Define a Constructor Function.js
@@ -18,4 +18,6 @@ and 2, respectively. Constructors follow a few conventions:
 Constructors are defined with a capitalized name to distinguish them from other functions that are not constructors.
 Constructors use the keyword this to set properties of the object they will create. Inside the constructor, this refers to the new object it will create.
 Constructors define properties and behaviors instead of returning a value as other functions might.
+
+Create a constructor, Dog, with properties name, color, and numLegs that are set to a string, a string, and a number, respectively.
 */
