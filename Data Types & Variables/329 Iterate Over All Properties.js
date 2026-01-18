87dbcba5730dd9cc985de6b4b6918b7c0d16cd51
Kevin Zhang <zhangzheng888@gmail.com>
2026-01-18 12:23:30 -0800

    Update Iterate Over All Properties JavaScript file

--- Data Types & Variables/329 Iterate Over All Properties.js
+++ Data Types & Variables/329 Iterate Over All Properties.js
@@ -34,3 +34,14 @@ console.log(prototypeProps);
 /*
 Add all of the own properties of beagle to the array ownProps. Add all of the prototype properties of Dog to the array prototypeProps.
 */
+
+function Dog(name) {
+  this.name = name;
+}
+
+Dog.prototype.numLegs = 4;
+
+let beagle = new Dog("Snoopy");
+
+let ownTestProps = [];
+let prototypeTestProps = [];
