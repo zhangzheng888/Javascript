85ab106b3350dd44c31e1334d4edd407d8130580
Kevin Zhang <zhangzheng888@gmail.com>
2026-01-25 17:20:34 -0800

    Update Understand the Constructor Property JavaScript file

--- Data Types & Variables/330 Understand the Constructor Property.js
+++ Data Types & Variables/330 Understand the Constructor Property.js
@@ -10,4 +10,16 @@ console.log(beagle.constructor === Dog);
 
 /*
 Both of these console.log calls would display true in the console.
+
+Note that the constructor property is a reference to the constructor function that created the instance. 
+The advantage of the constructor property is that it's possible to check for this property to find out what kind of object it is. 
+Here's an example of how this could be used:
 */
+
+function joinBirdFraternity(candidate) {
+  if (candidate.constructor === Bird) {
+    return true;
+  } else {
+    return false;
+  }
+}
