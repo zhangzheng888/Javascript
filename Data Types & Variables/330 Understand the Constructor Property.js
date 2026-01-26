b4f96b11e21b3c51785c0b7b760ecd779e316462
Kevin Zhang <zhangzheng888@gmail.com>
2026-01-26 12:03:54 -0800

    Update Understand the Constructor Property JavaScript file

--- Data Types & Variables/330 Understand the Constructor Property.js
+++ Data Types & Variables/330 Understand the Constructor Property.js
@@ -23,3 +23,11 @@ function joinBirdFraternity(candidate) {
     return false;
   }
 }
+
+/*
+Note: Since the constructor property can be overwritten (which will be covered in the next two challenges) 
+it’s generally better to use the instanceof method to check the type of an object.
+
+Write a joinDogFraternity function that takes a candidate parameter and, using the constructor property, 
+return true if the candidate is a Dog, otherwise return false.
+*/
