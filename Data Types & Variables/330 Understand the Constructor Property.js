bf4606728ba94781bdc3d711c197efa9b6779e56
Kevin Zhang <zhangzheng888@gmail.com>
2026-01-27 15:21:53 -0800

    Update Understand the Constructor Property JavaScript file

--- Data Types & Variables/330 Understand the Constructor Property.js
+++ Data Types & Variables/330 Understand the Constructor Property.js
@@ -31,3 +31,15 @@ it’s generally better to use the instanceof method to check the type of an obj
 Write a joinDogFraternity function that takes a candidate parameter and, using the constructor property, 
 return true if the candidate is a Dog, otherwise return false.
 */
+
+function Dog(name) {
+  this.name = name;
+}
+
+function joinDogFraternity(candidate) {
+  if (candidate.constructor === Dog) {
+    return true;
+  } else {
+    return false;
+  }
+}
