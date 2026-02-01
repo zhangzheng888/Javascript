d7403d16b4a7abb54b6f609fff8236db6c93e071
Kevin Zhang <zhangzheng888@gmail.com>
2026-02-01 11:39:02 -0800

    Update Change the Prototype to a New Object JavaScript file

--- Data Types & Variables/331 Change the Prototype to a New Object.js
+++ Data Types & Variables/331 Change the Prototype to a New Object.js
@@ -7,3 +7,16 @@ function Bird(name) {
 }
 
 Bird.prototype.numLegs = 2;
+
+/*
+This becomes tedious after more than a few properties.
+
+*/
+
+Bird.prototype.eat = function() {
+  console.log("nom nom nom");
+}
+
+Bird.prototype.describe = function() {
+  console.log("My name is " + this.name);
+}
