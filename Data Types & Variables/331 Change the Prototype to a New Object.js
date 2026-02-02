7e15abdfc35856a2ed11696f2b936f5cab12f3bd
Kevin Zhang <zhangzheng888@gmail.com>
2026-02-02 00:10:17 -0800

    Update Change the Prototype to a New Object JavaScript file

--- Data Types & Variables/331 Change the Prototype to a New Object.js
+++ Data Types & Variables/331 Change the Prototype to a New Object.js
@@ -20,3 +20,18 @@ Bird.prototype.eat = function() {
 Bird.prototype.describe = function() {
   console.log("My name is " + this.name);
 }
+
+/*
+A more efficient way is to set the prototype to a new object that already contains the properties.
+This way, the properties are added all at once:
+*/
+
+Bird.prototype = {
+  numLegs: 2, 
+  eat: function() {
+    console.log("nom nom nom");
+  },
+  describe: function() {
+    console.log("My name is " + this.name);
+  }
+};
