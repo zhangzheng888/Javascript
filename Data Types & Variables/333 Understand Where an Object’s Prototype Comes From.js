5772d757c8316dad6722f1d84385903361a15b61
Kevin Zhang <zhangzheng888@gmail.com>
2026-02-12 17:50:30 -0800

    Update Understand Where an Object's Prototype Comes From JavaScript file

--- Data Types & Variables/333 Understand Where an Object’s Prototype Comes From.js
+++ Data Types & Variables/333 Understand Where an Object’s Prototype Comes From.js
@@ -8,3 +8,9 @@ function Bird(name) {
 }
 
 let duck = new Bird("Donald");
+
+/*
+duck inherits its prototype from the Bird constructor function. You can show this relationship with the isPrototypeOf method:
+*/
+
+Bird.prototype.isPrototypeOf(duck);
