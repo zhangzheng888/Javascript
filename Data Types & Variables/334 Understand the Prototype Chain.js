1ff3e09977984609f12e634c0fc45a9aab7a7831
Kevin Zhang <zhangzheng888@gmail.com>
2026-02-18 23:27:33 -0800

    Update Understand the Prototype Chain JavaScript file

--- Data Types & Variables/334 Understand the Prototype Chain.js
+++ Data Types & Variables/334 Understand the Prototype Chain.js
@@ -19,3 +19,12 @@ Object.prototype.isPrototypeOf(Bird.prototype);
 
 let duck = new Bird("Donald");
 duck.hasOwnProperty("name");
+
+/*
+The hasOwnProperty method is defined in Object.prototype, which can be accessed by Bird.prototype, 
+which can then be accessed by duck. This is an example of the prototype chain. In this prototype chain, Bird is 
+the supertype for duck, while duck is the subtype. Object is a supertype for both Bird and duck. Object is a supertype 
+for all objects in JavaScript. Therefore, any object can use the hasOwnProperty method.
+
+Modify the code to show the correct prototype chain.
+*/
