15f79dcd5a9bb46d7cbe8f3c53f5f3bff49cc586
Kevin Zhang <zhangzheng888@gmail.com>
2026-02-21 19:14:51 -0800

    Create Use Inheritance So You Don't Repeat Yourself JavaScript file

---
+++ Data Types & Variables/335 Use Inheritance So You Don't Repeat Yourself.js
@@ -0,0 +1,7 @@
+/*
+There's a principle in programming called Don't Repeat Yourself (DRY). The reason repeated code is a problem is because any 
+change requires fixing code in multiple places. This usually means more work for programmers and more room for errors.
+
+Notice in the example below that the describe method is shared by Bird and Dog:
+*/
+
