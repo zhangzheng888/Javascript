75cf4c6d9f3e6e5242c53a562c30a77c391ae0a1
Kevin Zhang <zhangzheng888@gmail.com>
2026-03-02 19:51:41 -0800

    Update Use Inheritance So You Don't Repeat Yourself JavaScript file

--- Data Types & Variables/335 Use Inheritance So You Don't Repeat Yourself.js
+++ Data Types & Variables/335 Use Inheritance So You Don't Repeat Yourself.js
@@ -46,3 +46,32 @@ Dog.prototype = {
 /*
 The eat method is repeated in both Cat and Bear. Edit the code in the spirit of DRY by moving the eat method to the Animal supertype.
 */
+
+function Cat(name) {
+  this.name = name;
+}
+
+Cat.prototype = {
+  constructor: Cat,
+  eat: function() {
+    console.log("nom nom nom");
+  }
+};
+
+function Bear(name) {
+  this.name = name;
+}
+
+Bear.prototype = {
+  constructor: Bear,
+  eat: function() {
+    console.log("nom nom nom");
+  }
+};
+
+function Animal() { }
+
+Animal.prototype = {
+  constructor: Animal,
+
+};
