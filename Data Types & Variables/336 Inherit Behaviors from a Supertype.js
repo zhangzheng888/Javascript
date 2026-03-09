3dca2d6492033991217194ba71c7bd264560805b
Kevin Zhang <zhangzheng888@gmail.com>
2026-03-09 13:48:27 -0700

    Update Inherit Behaviors from a Supertype JavaScript file

--- Data Types & Variables/336 Inherit Behaviors from a Supertype.js
+++ Data Types & Variables/336 Inherit Behaviors from a Supertype.js
@@ -6,3 +6,11 @@ function Animal() { }
 Animal.prototype.eat = function() {
   console.log("nom nom nom");
 };
+
+/*
+This and the next challenge will cover how to reuse the methods of Animal inside Bird and Dog without defining them again. 
+It uses a technique called inheritance. This challenge covers the first step: make an instance of the supertype (or parent). 
+You already know one way to create an instance of Animal using the new operator:
+*/
+
+let animal = new Animal();
