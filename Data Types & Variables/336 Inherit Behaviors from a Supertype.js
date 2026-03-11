fe66caed608539c660dda52a1775a56ce74746dc
Kevin Zhang <zhangzheng888@gmail.com>
2026-03-11 15:19:48 -0700

    Update Inherit Behaviors from a Supertype JavaScript file

--- Data Types & Variables/336 Inherit Behaviors from a Supertype.js
+++ Data Types & Variables/336 Inherit Behaviors from a Supertype.js
@@ -13,11 +13,17 @@ It uses a technique called inheritance. This challenge covers the first step: ma
 You already know one way to create an instance of Animal using the new operator:
 */
 
-let animal = new Animal();
+let animalOG = new Animal();
 
 /*
 There are some disadvantages when using this syntax for inheritance, which are too complex for the scope of this challenge. 
 Instead, here's an alternative approach without those disadvantages:
 */
 
-let animal2 = Object.create(Animal.prototype);
+let animalNew = Object.create(Animal.prototype);
+
+/*
+Object.create(obj) creates a new object, and sets obj as the new object's prototype. Recall that the prototype is like the 
+"recipe" for creating an object. By setting the prototype of animal to be the prototype of Animal, you are effectively giving 
+the animal instance the same "recipe" as any other instance of Animal.
+*/
