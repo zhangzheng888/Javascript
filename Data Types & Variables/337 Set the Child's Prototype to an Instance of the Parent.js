2c18b38a846a11399c2245c90d9cf2cf66c44b92
Kevin Zhang <zhangzheng888@gmail.com>
2026-03-16 15:25:32 -0700

    Create Set the Child's Prototype to an Instance of the Parent JavaScript file

---
+++ Data Types & Variables/337 Set the Child's Prototype to an Instance of the Parent.js
@@ -0,0 +1,6 @@
+/*
+In the previous challenge you saw the first step for inheriting behavior from the supertype (or parent) Animal: making a new 
+instance of Animal.
+
+This challenge covers the next step: set the prototype of the subtype (or child)—in this case, Bird—to be an instance of Animal.
+*/
