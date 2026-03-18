ceb76685351f91f1588aa80c4cfe252107fd5a51
Kevin Zhang <zhangzheng888@gmail.com>
2026-03-17 20:00:52 -0700

    Update Set the Child's Prototype to an Instance of the Parent JavaScript file

--- Data Types & Variables/337 Set the Child's Prototype to an Instance of the Parent.js
+++ Data Types & Variables/337 Set the Child's Prototype to an Instance of the Parent.js
@@ -4,3 +4,5 @@ instance of Animal.
 
 This challenge covers the next step: set the prototype of the subtype (or child)—in this case, Bird—to be an instance of Animal.
 */
+
+Bird.prototype = Object.create(Animal.prototype);
