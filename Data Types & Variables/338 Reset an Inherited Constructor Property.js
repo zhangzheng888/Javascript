b1fede76ff66f642b206e459fff4af93e3ac7fb8
Kevin <zhangzheng888@gmail.com>
2026-04-05 21:56:45 -0700

    Update Reset an Inherited Constructor Property JavaScript file

--- Data Types & Variables/338 Reset an Inherited Constructor Property.js
+++ Data Types & Variables/338 Reset an Inherited Constructor Property.js
@@ -16,3 +16,18 @@ you can manually set the constructor property of Bird to the Bird object:
 
 Bird.prototype.constructor = Bird;
 duck.constructor;
+
+// Fix the code so duck.constructor and beagle.constructor return their respective constructors.
+
+function Animal() { }
+function Bird() { }
+function Dog() { }
+
+Bird.prototype = Object.create(Animal.prototype);
+Dog.prototype = Object.create(Animal.prototype);
+
+Bird.prototype.constructor = Bird;
+Dog.prototype.constructor = Dog;
+
+let duckNew = new Bird();
+let beagle = new Dog();
