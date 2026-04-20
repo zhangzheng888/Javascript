b3cb05767aadeb782973b35aa83328faf53c9f68
Kevin <zhangzheng888@gmail.com>
2026-04-20 16:55:36 -0700

    Update Add Methods After Inheritance JavaScript file

--- Data Types & Variables/339 Add Methods After Inheritance.js
+++ Data Types & Variables/339 Add Methods After Inheritance.js
@@ -1,5 +1,6 @@
 /*
-A constructor function that inherits its prototype object from a supertype constructor function can still have its own methods in addition to inherited methods.
+A constructor function that inherits its prototype object from a supertype constructor function can still have its own 
+methods in addition to inherited methods.
 
 For example, Bird is a constructor that inherits its prototype from Animal:
 */
@@ -12,3 +13,8 @@ Animal.prototype.eat = function() {
 function Bird() { }
 Bird.prototype = Object.create(Animal.prototype);
 Bird.prototype.constructor = Bird;
+
+/*
+In addition to what is inherited from Animal, you want to add behavior that is unique to Bird objects. Here, Bird will 
+get a fly() function. Functions are added to Bird's prototype the same way as any constructor function:
+*/
