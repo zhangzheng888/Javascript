e44ba21b0f12a8f064e331ffa259410930de6658
Kevin <zhangzheng888@gmail.com>
2026-04-29 21:12:00 -0700

    Create Override Inherited Methods JavaScript file

---
+++ Data Types & Variables/340 Override Inherited Methods.js
@@ -0,0 +1,6 @@
+/*
+In previous lessons, you learned that an object can inherit its behavior (methods) from another object by 
+referencing its prototype object:
+*/
+
+ChildObject.prototype = Object.create(ParentObject.prototype);
