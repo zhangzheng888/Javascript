58f7369b20f1e124eaf19cb8ef49135aec15363c
Kevin <zhangzheng888@gmail.com>
2026-05-01 22:34:25 -0700

    Update Override Inherited Methods JavaScript file

--- Data Types & Variables/340 Override Inherited Methods.js
+++ Data Types & Variables/340 Override Inherited Methods.js
@@ -9,5 +9,21 @@ ChildObject.prototype = Object.create(ParentObject.prototype);
 Then the ChildObject received its own methods by chaining them onto its prototype:
 */
 
-ChildObject.prototype.methodName = function() {...};
+// ChildObject.prototype.methodName = function() {...};
 
+/*
+It's possible to override an inherited method. It's done the same way - by adding a method to ChildObject.prototype 
+using the same method name as the one to override. Here's an example of Bird overriding the eat() method inherited from Animal:
+*/
+
+function Animal() { }
+Animal.prototype.eat = function() {
+  return "nom nom nom";
+};
+function Bird() { }
+
+Bird.prototype = Object.create(Animal.prototype);
+
+Bird.prototype.eat = function() {
+  return "peck peck peck";
+};
