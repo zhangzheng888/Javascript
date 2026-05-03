4a3b6d0f6ba36cdd5db99400c2c0dcf771250831
Kevin <zhangzheng888@gmail.com>
2026-05-02 22:12:39 -0700

    Update Override Inherited Methods JavaScript file

--- Data Types & Variables/340 Override Inherited Methods.js
+++ Data Types & Variables/340 Override Inherited Methods.js
@@ -27,3 +27,12 @@ Bird.prototype = Object.create(Animal.prototype);
 Bird.prototype.eat = function() {
   return "peck peck peck";
 };
+
+/*
+If you have an instance let duck = new Bird(); and you call duck.eat(), this is how JavaScript looks for the method on the prototype chain of duck:
+
+duck => Is eat() defined here? No.
+Bird => Is eat() defined here? => Yes. Execute it and stop searching.
+Animal => eat() is also defined, but JavaScript stopped searching before reaching this level.
+Object => JavaScript stopped searching before reaching this level.
+*/
