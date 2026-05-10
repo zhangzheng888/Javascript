832c4a13e84c865660cff0da30717ae03252e29d
Kevin <zhangzheng888@gmail.com>
2026-05-09 22:46:15 -0700

    Create Use a Mixin to Add Common Behavior Between Unrelated Objects JavaScript file

---
+++ Data Types & Variables/341 Use a Mixin to Add Common Behavior Between Unrelated Objects.js
@@ -0,0 +1,7 @@
+/*
+As you have seen, behavior is shared through inheritance. However, there are cases when inheritance is not the best 
+solution. Inheritance does not work well for unrelated objects like Bird and Airplane. They can both fly, but a Bird is
+ not a type of Airplane and vice versa.
+
+For unrelated objects, it's better to use mixins. A mixin allows other objects to use a collection of functions.
+*/
