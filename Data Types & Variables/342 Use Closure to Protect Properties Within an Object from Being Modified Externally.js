73ea2679d9d2a30a0161c76ec88c2724bfcf58f6
Kevin <zhangzheng888@gmail.com>
2026-05-25 15:21:06 -0700

    Create Use Closure to Protect Properties Within an Object from Being Modified Externally JavaScript file

---
+++ Data Types & Variables/342 Use Closure to Protect Properties Within an Object from Being Modified Externally.js
@@ -0,0 +1,6 @@
+/*
+In the previous challenge, bird had a public property name. It is considered public because it can be 
+accessed and changed outside of bird's definition.
+*/
+
+bird.name = "Duffy";
