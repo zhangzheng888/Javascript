dd75d3832c322f3f82638c6f2a41d957cde87ca9
Kevin <zhangzheng888@gmail.com>
2026-05-28 00:42:29 -0700

    Update Use Closure to Protect Properties Within an Object from Being Modified Externally JavaScript file

--- Data Types & Variables/342 Use Closure to Protect Properties Within an Object from Being Modified Externally.js
+++ Data Types & Variables/342 Use Closure to Protect Properties Within an Object from Being Modified Externally.js
@@ -13,3 +13,13 @@ The simplest way to make this public property private is by creating a variable 
 This changes the scope of that variable to be within the constructor function versus available globally. This way, 
 the variable can only be accessed and changed by methods also within the constructor function.
 */
+
+function Bird() {
+  let hatchedEgg = 10;
+
+  this.getHatchedEggCount = function() { 
+    return hatchedEgg;
+  };
+}
+let ducky = new Bird();
+ducky.getHatchedEggCount();
