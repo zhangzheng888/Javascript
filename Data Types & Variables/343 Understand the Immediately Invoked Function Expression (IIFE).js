9fed36e454d4aecc67d5910f13bda75db45bc07c
Kevin <zhangzheng888@gmail.com>
2026-06-02 22:34:00 -0700

    Update Understand the Immediately Invoked Function Expression (IIFE) JavaScript file

--- Data Types & Variables/343 Understand the Immediately Invoked Function Expression (IIFE).js
+++ Data Types & Variables/343 Understand the Immediately Invoked Function Expression (IIFE).js
@@ -10,4 +10,8 @@ This is an anonymous function expression that executes right away, and outputs C
 Note that the function has no name and is not stored in a variable. The two parentheses () at the end of the function 
 expression cause it to be immediately executed or invoked. This pattern is known as an immediately invoked function 
 expression or IIFE.
+
+Rewrite the function makeNest and remove its call so instead it's an anonymous immediately invoked function 
+expression (IIFE).
 */
+
