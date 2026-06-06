b1011e98bfe18160a51cd902e1660f2e7d5347f4
Kevin <zhangzheng888@gmail.com>
2026-06-06 13:07:07 -0700

    Update Use an IIFE to Create a Module JavaScript file

--- Data Types & Variables/344 Use an IIFE to Create a Module.js
+++ Data Types & Variables/344 Use an IIFE to Create a Module.js
@@ -2,3 +2,14 @@
 An immediately invoked function expression (IIFE) is often used to group related functionality into a single 
 object or module. For example, an earlier challenge defined two mixins:
 */
+
+function glideMixin(obj) {
+  obj.glide = function() {
+    console.log("Gliding on the water");
+  };
+}
+function flyMixin(obj) {
+  obj.fly = function() {
+    console.log("Flying, wooosh!");
+  };
+}
