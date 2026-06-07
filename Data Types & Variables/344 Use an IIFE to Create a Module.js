05b6623bda2d7439d011536ae94a28acaee4046b
Kevin <zhangzheng888@gmail.com>
2026-06-07 14:28:15 -0700

    Update Use an IIFE to Create a Module JavaScript file

--- Data Types & Variables/344 Use an IIFE to Create a Module.js
+++ Data Types & Variables/344 Use an IIFE to Create a Module.js
@@ -13,3 +13,20 @@ function flyMixin(obj) {
     console.log("Flying, wooosh!");
   };
 }
+
+// We can group these mixins into a module as follows:
+
+let motionModule = (function () {
+  return {
+    glideMixin: function(obj) {
+      obj.glide = function() {
+        console.log("Gliding on the water");
+      };
+    },
+    flyMixin: function(obj) {
+      obj.fly = function() {
+        console.log("Flying, wooosh!");
+      };
+    }
+  }
+})();
