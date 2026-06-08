6f2e756da0f7e3afbf6c5f02bde7a95309976aa0
Kevin <zhangzheng888@gmail.com>
2026-06-08 15:19:15 -0700

    Update Use an IIFE to Create a Module JavaScript file

--- Data Types & Variables/344 Use an IIFE to Create a Module.js
+++ Data Types & Variables/344 Use an IIFE to Create a Module.js
@@ -30,3 +30,10 @@ let motionModule = (function () {
     }
   }
 })();
+
+/*
+Note that you have an immediately invoked function expression (IIFE) that returns an object motionModule. 
+This returned object contains all of the mixin behaviors as properties of the object. The advantage of the 
+module pattern is that all of the motion behaviors can be packaged into a single object that can then be used by 
+other parts of your code. Here is an example using it:
+*/
