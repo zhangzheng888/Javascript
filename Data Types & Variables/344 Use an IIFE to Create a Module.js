106214717fedc90e32440aed866f59d925a53c00
Kevin <zhangzheng888@gmail.com>
2026-06-11 01:08:17 -0700

    Update Use an IIFE to Create a Module JavaScript file

--- Data Types & Variables/344 Use an IIFE to Create a Module.js
+++ Data Types & Variables/344 Use an IIFE to Create a Module.js
@@ -42,3 +42,14 @@ motionModule.glideMixin(duck);
 duck.glide();
 
 // Create a module named funModule to wrap the two mixins isCuteMixin and singMixin. funModule should return an object.
+
+let isCuteMixin = function(obj) {
+  obj.isCute = function() {
+    return true;
+  };
+};
+let singMixin = function(obj) {
+  obj.sing = function() {
+    console.log("Singing to an awesome tune");
+  };
+};
