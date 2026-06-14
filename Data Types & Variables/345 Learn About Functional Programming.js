eb98acfd70ed2a65434ac6f52db8eee6eb5eb556
Kevin <zhangzheng888@gmail.com>
2026-06-13 22:59:52 -0700

    Create Learn About Functional Programming JavaScript file

---
+++ Data Types & Variables/345 Learn About Functional Programming.js
@@ -0,0 +1,14 @@
+/*
+Functional programming is a style of programming where solutions are simple, isolated functions, without any side 
+effects outside of the function scope: INPUT -> PROCESS -> OUTPUT
+
+Functional programming is about:
+
+Isolated functions - there is no dependence on the state of the program, which includes global variables that are 
+subject to change
+
+Pure functions - the same input always gives the same output
+
+Functions with limited side effects - any changes, or mutations, to the state of the program outside the function 
+are carefully controlled
+*/
