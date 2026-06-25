a211e2a44a6c6172ff9968e69566c620d9590ee3
Kevin <zhangzheng888@gmail.com>
2026-06-24 17:42:44 -0700

    Update Learn About Functional Programming JavaScript file

--- Data Types & Variables/346 Understand Functional Programming Terminology.js
+++ Data Types & Variables/346 Understand Functional Programming Terminology.js
@@ -24,3 +24,32 @@ Note that the getTea function has been modified so it now takes a function as th
 
 Note: The data (the number of cups of tea) is supplied as the last argument.
 */
+
+// Function that returns a string representing a cup of green tea
+const prepareGreenTea = () => 'greenTea';
+
+// Function that returns a string representing a cup of black tea
+const prepareBlackTea = () => 'blackTea';
+
+/*
+Given a function (representing the tea type) and number of cups needed, the
+following function returns an array of strings (each representing a cup of
+a specific type of tea).
+*/
+const getTea = (prepareTea, numOfCups) => {
+  const teaCups = [];
+
+  for(let cups = 1; cups <= numOfCups; cups += 1) {
+    const teaCup = prepareTea();
+    teaCups.push(teaCup);
+  }
+  return teaCups;
+};
+
+const tea4GreenTea = null;
+const tea4BlackTea = null;
+
+console.log(
+  tea4GreenTea,
+  tea4BlackTea
+);
